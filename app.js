dfd23114ceb8b4cc2c1e93f9c19d3e3ae1b80dc8
Aayush Juyal <97756925+aj12-houdini@users.noreply.github.com>
2023-07-14 14:48:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 
-//Requiring all the necessart modules 
+//Requiring all the necessary modules 
 require("dotenv").config();
 const express = require("express");
 const app = express();
 
-//Making a get request to the root route 
+//Making a GET request to the root route 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-//Requiring the OAuth Authentication module  
+//Requiring the OAuth Authorization module  
 const authorize = require("./src/authorization/auth");
 
 //Requiring the AutoReply module 
 const autoRespondEmail = require("./src/main/auto_reply");
 
-//Executing the AutoReply module given that the user is authenticated 
+//Executing the AutoReply module given that the user is authenticated and authorized
 authorize().then(autoRespondEmail).catch(console.error);
 
-//Listenning and starting the server on port specified in the environment variable 
+//Listenning and starting the server on port specified in the .env file 
 app.listen(process.env.PORT, () => {
   console.log("Server started on port: " + process.env.PORT);
 });
